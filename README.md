c37ed56e4b8ce0f5d4eaa69a80d4e7a3a3d95fb5
munergenis <154993411+munergenis@users.noreply.github.com>
2024-07-31 00:51:39 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# AirBnb clone
+
+AirBnb clone project from Scrimba, made with React + Tailwind. Bundled with Vite.
+
+Project to refresh React and Tailwind concepts.
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import airbnbLogo from 'assets/airbnb-logo.png'
+
+const Navbar = () => {
+  return (
+    <>
+      <img src={airbnbLogo} alt="" />
+      <h1>Navbar</h1>
+    </>
+  )
+}
+
+export default Navbar
